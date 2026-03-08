04995c9b532cd3f07e61f6343fcf55320df06006
Uju <56465463+black-pepper@users.noreply.github.com>
2026-03-08 19:48:35 +0900

    init

---
+++ src/main/java/com/luckydrop/api/LuckydropApiApplication.java
@@ -0,0 +1,13 @@
+package com.luckydrop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckydropApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckydropApiApplication.class, args);
+	}
+
+}
