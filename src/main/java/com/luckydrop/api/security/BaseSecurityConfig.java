d0e58bf652e8b9804723905bf64af4e1627596bd
Uju <56465463+black-pepper@users.noreply.github.com>
2026-04-28 08:18:11 +0000

    fix error

--- src/main/java/com/luckydrop/api/security/BaseSecurityConfig.java
+++ src/main/java/com/luckydrop/api/security/BaseSecurityConfig.java
@@ -1,5 +1,11 @@
 package com.luckydrop.api.security;
 
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.jwk.source.JWKSourceBuilder;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
@@ -16,6 +22,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 abstract class BaseSecurityConfig {
@@ -56,13 +64,14 @@ abstract class BaseSecurityConfig {
         String issuerUri = securityProperties.getJwt().getIssuerUri();
         String jwkSetUri = securityProperties.getJwt().getJwkSetUri();
 
-        NimbusJwtDecoder jwtDecoder = StringUtils.hasText(jwkSetUri)
-                ? NimbusJwtDecoder.withJwkSetUri(jwkSetUri)
-                .jwsAlgorithm(SignatureAlgorithm.ES256)
-                .build()
-                : NimbusJwtDecoder.withIssuerLocation(issuerUri)
-                .jwsAlgorithm(SignatureAlgorithm.ES256)
-                .build();
+        NimbusJwtDecoder jwtDecoder;
+        if (StringUtils.hasText(jwkSetUri)) {
+            jwtDecoder = buildJwkSetUriDecoder(jwkSetUri);
+        } else {
+            jwtDecoder = NimbusJwtDecoder.withIssuerLocation(issuerUri)
+                    .jwsAlgorithm(SignatureAlgorithm.ES256)
+                    .build();
+        }
 
         OAuth2TokenValidator<Jwt> validator = JwtValidators.createDefaultWithIssuer(issuerUri);
         if (StringUtils.hasText(securityProperties.getJwt().getAudience())) {
@@ -73,5 +82,23 @@ abstract class BaseSecurityConfig {
         }
         jwtDecoder.setJwtValidator(validator);
         return jwtDecoder;
+    }
+
+    private NimbusJwtDecoder buildJwkSetUriDecoder(String jwkSetUri) {
+        try {
+            JWKSource<SecurityContext> jwkSource = JWKSourceBuilder
+                    .create(new URL(jwkSetUri))
+                    .cache(15 * 60 * 1000L, 5 * 60 * 1000L)
+                    .retrying(true)
+                    .outageTolerant(true)
+                    .rateLimited(30 * 1000L)
+                    .build();
+
+            DefaultJWTProcessor<SecurityContext> processor = new DefaultJWTProcessor<>();
+            processor.setJWSKeySelector(new JWSVerificationKeySelector<>(JWSAlgorithm.ES256, jwkSource));
+            return new NimbusJwtDecoder(processor);
+        } catch (MalformedURLException e) {
+            throw new IllegalStateException("Invalid JWK Set URI: " + jwkSetUri, e);
+        }
     }
 }
