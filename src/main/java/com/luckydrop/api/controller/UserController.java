7d9d9045110e998798612cf0d92eb33465f52e4b
Uju <56465463+black-pepper@users.noreply.github.com>
2026-04-28 05:49:54 +0000

    fix user update error

--- src/main/java/com/luckydrop/api/controller/UserController.java
+++ src/main/java/com/luckydrop/api/controller/UserController.java
@@ -6,11 +6,13 @@ import com.luckydrop.api.domain.user.dto.UserRequest;
 import com.luckydrop.api.service.CurrentUserService;
 import com.luckydrop.api.service.UserWithdrawalService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,7 +29,7 @@ public class UserController {
     }
 
     @PutMapping("/user")
-    public ResponseEntity<ApiResponse<UserInfo>> updateCurrentUser(UserRequest request) {
+    public ResponseEntity<ApiResponse<UserInfo>> updateCurrentUser(@RequestBody @Valid UserRequest request) {
         currentUserService.updateCurrentUserEntity(request);
         return ResponseEntity.ok(ApiResponse.ok(null));
     }
