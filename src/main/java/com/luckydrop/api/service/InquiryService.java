f7b075806c29f0e7a134e32cc967acd6feaabb09
Uju <56465463+black-pepper@users.noreply.github.com>
2026-06-04 07:52:56 +0000

    feat: link inquiries to current user

--- src/main/java/com/luckydrop/api/service/InquiryService.java
+++ src/main/java/com/luckydrop/api/service/InquiryService.java
@@ -4,20 +4,26 @@ import com.luckydrop.api.domain.inquiry.dto.InquiryRequest;
 import com.luckydrop.api.domain.inquiry.entity.Inquiry;
 import com.luckydrop.api.domain.inquiry.entity.InquiryStatus;
 import com.luckydrop.api.domain.inquiry.repository.InquiryRepository;
+import com.luckydrop.api.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class InquiryService {
     private final InquiryRepository inquiryRepository;
+    private final CurrentUserService currentUserService;
 
+    @Transactional
     public void create(InquiryRequest request) {
+        User user = currentUserService.getCurrentUserEntityOptional().orElse(null);
         inquiryRepository.save(Inquiry.builder()
                 .type(request.getType())
                 .title(request.getTitle())
                 .content(request.getContent())
                 .status(InquiryStatus.PENDING)
+                .user(user)
                 .build());
     }
 }
